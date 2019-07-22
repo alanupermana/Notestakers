22bb52aa151b1b13b423f2258de36b76da182b61
alanupermana <alanupermana@gmail.com>
2019-07-22 09:31:03 +0700

    Splash,Onboarding,login,signup

--- app/src/main/java/com/example/notestakers/Login.java
+++ app/src/main/java/com/example/notestakers/Login.java
@@ -1,13 +1,31 @@
 package com.example.notestakers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    TextView tvSignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_login );
+
+        tvSignup = findViewById(R.id.tvSignup);
+        tvSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Login.this,SignUp.class);
+                startActivity(i);
+            }
+        });
     }
+
+
+
 }
--- app/src/main/java/com/example/notestakers/SignUp.java
+++ app/src/main/java/com/example/notestakers/SignUp.java
@@ -1,13 +1,27 @@
 package com.example.notestakers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SignUp extends AppCompatActivity {
+
+    TextView tvSignin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_sign_up );
+
+        tvSignin = findViewById(R.id.tvSignin);
+        tvSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SignUp.this,Login.class);
+                startActivity(i);
+            }
+        });
     }
 }
